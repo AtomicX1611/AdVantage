71fe6219750febd10036c43dd1f877ffa2e8963e
Rajeevlochan <rajeevlochanperla@gmail.com>
2025-03-18 16:35:12 +0530

    user name added in chat app

--- Advantage/routes/charRoutes.js
+++ Advantage/routes/charRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { conversation } from "../models/Conversation.js";
 import { fetchConversations } from "../models/Conversation.js";
 import { senderList } from "../models/Conversation.js";
+import { findUserByEmail } from "../models/User.js";
 
 export const chatRoutes = express();
 
@@ -10,12 +11,12 @@ chatRoutes.get("/buyerInbox", (req, res) => {
     if (req.isAuthenticated() && req.user.role == "buyer") {
         let senders = senderList(req.user.email, req.user.role);
         console.log("senders: ",senders);
-        res.render("buyerChat.ejs", { isLogged: true, senders: senders ,myAccount:req.user.email});
+        res.render("buyerChat.ejs", { isLogged: true, senders: senders ,myAccount:req.user.email,myUsername:findUserByEmail(req.user.email).username});
     }
     else {
         res.send("No data!! please login")
     }
-})
+});
 
 chatRoutes.get("/conversation",(req,res)=>{
     let sender=req.query.sender;
